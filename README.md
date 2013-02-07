07af3a5f91e70adc740b405621e69b100c6a4ef9
0xAX <testtest.89@mail.ru>
2013-02-07 19:41:08 +0600

    Initial release

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
 lager_ybot_backend
-==================
+===================
 
-lager_ybot_backend - receive your lager error messages via xmpp,irc,campfire and other im
+lager_ybot_backend - is Ybot's backend for Basho Lager logging framework. 
+It allows to receive lager error messages in your favorite messenger in real time.
+It's build on top of two libraries:
+
+  * Basho [lager](https://github.com/basho/lager) - A logging framework for Erlang/OTP
+  * [Ybot](https://github.com/0xAX/Ybot) - chat robot
+
+The principle of operation is simple: You have runned Ybot in your machine which sits in some
+chat rooms. Also you have some erlang project's which logging with lager.
+
+It supports resending error lagers messages to:
+
+  * IRC
+  * XMPP
+  * 37signals campfire chat.
+  * Google talk.
+  * HipChat.
+
+Configuring and usage
+=======================
+
+Just include ybot backend into your project with rebar:
+
+```erlang
+{lager_ybot_backend, 
+	".*", {git, "https://github.com/0xAX/lager_ybot_backend.git", "master"}}
+```
+
+And configure lager:
+
+```erlang
+{lager, [
+  {handlers, [
+    {lager_ybot_backend, [
+      {name,        "lager_amqp_backend"},
+      {level,       error},
+      {ybot_host, "http://localhost"},
+      {ybot_port, 8080}
+    ]}
+  ]}
+]}
+```
+
+After that it will sends error logging message to all chats are running your Ybot.
+
+Author
+========
+
+[@0xAX](https://twitter.com/0xAX). 
+
+
+
---
+++ src/lager_ybot_backend.erl
@@ -0,0 +1,88 @@
+-module(lager_ybot_backend).
+
+-behaviour(gen_event).
+
+%% api
+-export([init/1, handle_call/2, handle_event/2, handle_info/2, terminate/2, code_change/3]).
+
+%% test api
+-export([test/0]).
+
+%% internal state
+-record(state, {
+		name = "" :: string(),
+		% ybot http host
+  		host = "" :: string(),
+  		% ybot http port
+  		port = 0 :: integer(),
+  		% log level
+  		level
+}).
+
+init(Params) ->
+	% start inets
+	inets:start(),
+	% get ybot host from config
+	YbotHost = config(ybot_host, Params, "http://localhost"),
+	% get ybot port from config
+	YbotPort = config(ybot_port, Params, 8080),
+	% Get name
+	Name = config(name, Params, lager_ybot_backend),
+	% Get log level
+	Level = config(level, Params, error),
+	% init state
+	{ok, #state{name = Name, level = Level, host = YbotHost, port = YbotPort}}.
+	
+handle_call(_Request, State) ->
+  {ok, ok, State}.
+  
+%% @doc log messages  
+handle_event({log, LagerMessage}, #state{ host = Host, port = Port} = State) ->
+	% Make url
+	Url = Host ++ ":" ++ integer_to_list(Port),
+	case lager_msg:severity(LagerMessage) of
+		error ->
+			% Make message
+			Message = lager_msg:message(LagerMessage),
+			% send log error
+			httpc:request(put, {Url, [], [], Message}, [], []);
+		_ ->
+			% do nothing
+			pass
+	end,
+	% return state
+	{ok, State};
+	
+handle_event(Event, State) ->
+  	io:format("Event ~p~n", [Event]),
+    {ok, State}.
+    
+handle_info(_Info, State) ->
+  {ok, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+	
+%% @doc Get parameters from config
+config(C, Params, Default) ->
+  case lists:keyfind(C, 1, Params) of
+    {C, Value} -> 
+    	% return value
+    	Value;
+    _ -> 
+    	% not find. return default
+    	Default
+  end.
+  
+%% @doc TEST api
+test() ->
+	application:load(lager),
+	application:set_env(lager, handlers, [{lager_console_backend, debug}, {lager_ybot_backend, []}]),
+	application:set_env(lager, error_logger_redirect, false),
+	application:start(lager),
+	lager:log(info, self(), "Test INFO message"),
+    lager:log(debug, self(), "Test DEBUG message"),
+    lager:log(error, self(), "Test ERROR message").
